baa28d3d4b05698def7becdf66d6886a263a71e5
Cameron Zaas <cameronzaas@gmail.com>
2015-09-10 20:57:50 -0700

    Static file generator

---
+++ main.js
@@ -0,0 +1,48 @@
+'use strict';
+var fs = require('fs-extra');
+
+// example
+var buildList = {
+	folderName: 'parent-folder', // This will create an index file by default
+	filesArray: ['index', 'about-us', 'surgeons', 'contact', 'specials'], // file names will be given the templates file extension
+	template: 'template.cshtml', // include directory and extension
+	destinationDir: 'static/' // WARNING: this will overwrite files that exist 
+};
+
+
+fileArrayLoop(buildList.filesArray, buildFile);
+
+
+function buildFile(fileName){
+	
+	var folderName = buildList.folderName;
+	
+	var dest = buildList.destinationDir;
+	var template = buildList.template;
+	var extension = '.' + template.split('.')[1]; 
+	
+	var newFile = dest + fileName + extension;
+	
+	// template, directory + filename;
+	fs.copy(template, newFile, function(err){
+		if(err) console.error(err);
+		
+		console.log('File generated: ' + fileName + extension);
+	});
+}
+
+
+// Need to: loop over file names and be able to call a function on that file
+function fileArrayLoop(arr, func){
+	if(!Array.isArray(arr)){ return; }
+	
+	var arrayContainsIndex = arr.indexOf('index') > -1;
+	
+	if(!arrayContainsIndex){
+		arr.push('index');
+	}
+	
+	for(var i = 0; i < arr.length; i++){
+		func(arr[i]); // will run function with string of file name passed to it
+	}
+}
