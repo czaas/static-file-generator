d0ca947ab4515d7843553b7909c8b59232e1dc0b
Cameron Zaas <cameronzaas@gmail.com>
2015-09-12 14:47:04 -0700

    reorganized my app. Utsing gulp as a task runner.

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+	bs = require('browser-sync'),
+	reload = bs.reload,
+	scss = require('gulp-sass'),
+	html = require('gulp-html-minifier');
+	
+gulp.task('css', function(){
+	gulp.src('./_interface/scss/*.scss')
+	.pipe(scss().on('err', scss.logError))
+	.pipe(gulp.dest('./_interface/css/'))
+	.pipe(reload({stream: true}));
+});
+
+gulp.task('html', function() {
+  gulp.src('./_interface/html/*.html')
+    .pipe(html({collapseWhitespace: true}))
+    .pipe(gulp.dest('./_interface/'))
+	.pipe(reload({stream: true}));
+});
+
+gulp.task('watch', function(){
+	bs.init({
+		server: './_interface/'	
+	});
+	
+	gulp.watch('./_interface/scss/*.scss', ['css']);
+	//gulp.watch('/js/*.js', ['js']);
+	gulp.watch('./_interface/html/*.html', ['html']);
+})
+
+gulp.task('default', ['watch']);
